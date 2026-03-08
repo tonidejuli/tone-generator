b025d430c36e926f3812590fd3be2d083399c3b7
tonidejuli <220539207+tonidejuli@users.noreply.github.com>
2026-03-08 14:23:44 -0300

    Update tonegenerator.js

--- tonegenerator.js
+++ tonegenerator.js
@@ -2,7 +2,29 @@ const freqSlider = document.getElementById('ifreq')
 const freqValue = document.getElementById('freq-value')
 const volSlider = document.getElementById('ivolume')
 const volValue = document.getElementById('volume-value')
-const waveSelect = document.getElementById('waveform')
+const waveSelect = document.getElementById('iwaveform')
+const playPauseBtn = document.getElementById('play-pause')
+
+let audioContext = null
+let osc = null
+let ganho = null
+
+function initContext(){
+    if(audioContext) return
+
+    audioContext = new AudioContext()
+    ganho = audioContext.createGain()
+    ganho.gain.value = 0.5
+
+    osc = audioContext.createOscillator()
+    osc.type = "sine"
+    osc.frequency.value = 440
+
+    osc.connect(ganho)
+    ganho.connect(audioContext.destination)
+
+    osc.start()
+}
 
 freqSlider.addEventListener('input', ()=>{
     freqValue.textContent = Number(freqSlider.value)
@@ -10,4 +32,27 @@ freqSlider.addEventListener('input', ()=>{
 
 volSlider.addEventListener('input', ()=>{
     volValue.textContent = Number(volSlider.value)
+})
+
+waveSelect.addEventListener('change', ()=>{
+    console.log(waveSelect.value)
+})
+
+let isPlaying = false
+
+playPauseBtn.addEventListener('click', ()=> {
+    isPlaying = !isPlaying
+
+    if(isPlaying){
+        initContext()
+        audioContext.resume()
+        playPauseBtn.textContent = "Pausar"
+        console.log('TOCANDO')
+    }  else {
+        if(audioContext){
+            audioContext.suspend()
+        }
+        console.log('PAUSADO')
+        playPauseBtn.textContent = 'Play'
+    }
 })
